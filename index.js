80fd4c55847efd1f09c697509ac5bb21831674a8
kichalomex <kichalo23con@hotmail.com>
2023-03-29 18:33:45 -0600

    Update index.js

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ const unknownPath = (request,response) =>{
     })
 }
 app.use (unknownPath)
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 //app.listen(PORT)
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`);
